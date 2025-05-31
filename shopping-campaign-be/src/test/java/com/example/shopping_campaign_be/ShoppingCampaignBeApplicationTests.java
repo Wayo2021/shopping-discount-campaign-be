edfe2421b741f2c4706afcd66236c3ea3c24c2b4
LEARNER\safe2 <studentpig416@gmail.com>
2025-05-31 21:10:23 +0700

    Add project

---
+++ shopping-campaign-be/src/test/java/com/example/shopping_campaign_be/ShoppingCampaignBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopping_campaign_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCampaignBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
